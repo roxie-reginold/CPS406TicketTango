b79a0cfaa4642ed2c1a564d1a16807965d41b642
Riddhima <60081089+itzmeppl@users.noreply.github.com>
2023-03-26 23:45:45 -0400

    Add files via upload

--- Login.java
+++ Login.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Login {
     public JPanel Login;
@@ -14,7 +18,38 @@ public class Login {
     private JButton signUpButton;
     private JButton loginButton;
 
+    //variables from CreateAccount class
+    private Map<String, String> customers;
+    private File file;
+    private String fileName="UserDatabase.txt";
+    //
+
     public Login() {
+
+        String emailEntry = userNameIn.getText();
+        char[] passwordEntry = passwordField1.getPassword();
+
+        public void tangoCustomers(File file, HashMap <String, String> customers){
+            this.customers = new HashMap<String, String>();
+            this.file = new File(fileName);
+            try {
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    String[] parts = line.split(",");
+                    if (parts.length < 2) {
+                        System.out.println("Invalid input line: " + line);
+                        continue;
+                    }
+                    String email = parts[0];
+                    String password = parts[1];
+                    customers.put(email,password);
+                }
+                scanner.close();
+            } catch (IOException e) {
+                System.out.println("An error occurred while reading from the file: " + e.getMessage());
+            }
+        }
 
 
         loginButton.addActionListener(new ActionListener() {
@@ -54,14 +89,14 @@ public class Login {
         });
     }
 
-    /*public static void main(String[] args) {
-        JFrame startFrame = new JFrame("Login");
-        startFrame.setContentPane(new Login().Login);
-        startFrame.setPreferredSize(new Dimension(300, 400));
-        startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        startFrame.pack();
-        startFrame.setVisible(true);
-    }*/
+    public static void main(String[] args) {
+//        JFrame startFrame = new JFrame("Login");
+//        startFrame.setContentPane(new Login().Login);
+//        startFrame.setPreferredSize(new Dimension(300, 400));
+//        startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        startFrame.pack();
+//        startFrame.setVisible(true);
+    }
 
 
 }
